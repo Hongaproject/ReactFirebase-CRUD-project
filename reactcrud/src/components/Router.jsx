c0ba81272dd6bc7c45788ca29f1f4b325189319e
Hongaproject <tkwlscjq99@gmail.com>
2023-08-28 00:51:19 +0900

    라우터 정리

---
+++ reactcrud/src/components/Router.jsx
@@ -0,0 +1,9 @@
+// 강의서 react-router-dom 5.3.0 버전을 사용해서 사용.
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+// Router을 받아와야해서 function Router으로 만들면 중복으로 인식해서 오류가 발생
+export default () => {
+    return(
+        <div></div>
+    );
+}
--- reactcrud/src/firebase.jsx
+++ reactcrud/src/firebase.jsx
@@ -1,3 +1,5 @@
+// firebase 9.6.1 버전을 사용
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
