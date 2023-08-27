f70a29db3bed0adbfffc35a31b2c0f9d26a5abb0
Hongaproject <tkwlscjq99@gmail.com>
2023-08-27 23:38:06 +0900

    env

--- reactcrud/src/firebase.jsx
+++ reactcrud/src/firebase.jsx
@@ -9,17 +9,19 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4CcvriWWG0mZ2ZEfc9-3cfc8gcbnCRNI",
-  authDomain: "crud-twitter.firebaseapp.com",
-  projectId: "crud-twitter",
-  storageBucket: "crud-twitter.appspot.com",
-  messagingSenderId: "765687110584",
-  appId: "1:765687110584:web:cb034e1700c0546f114d4d"
+  apiKey: process.env.REACT_APP_API_KEY ,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-// 자동으로 firebaseConfig에 있는 것들을 생성해준다. 
+// 자동으로 설정을 셋팅해준다.
 
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
+
+
 
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
