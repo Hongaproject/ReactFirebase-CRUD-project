0f5168d4ffd50e371e9c01744a8a5e321aea5511
Hongaproject <tkwlscjq99@gmail.com>
2023-08-31 20:53:04 +0900

    nav바 및 profile서 로그아웃 버튼 제작

--- reactcrud/src/components/Router.jsx
+++ reactcrud/src/components/Router.jsx
@@ -1,8 +1,10 @@
 // 강의서 react-router-dom 5.3.0 버전을 사용해서 사용.
 import { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 // Router을 받아와야해서 function Router으로 만들면 중복으로 인식해서 오류가 발생
 const AppRouter = ({ islogin }) => {
@@ -10,12 +12,17 @@ const AppRouter = ({ islogin }) => {
     // Router 파일은 routes부분만 보여주기 위해 사용하려고 App파일서 islogin을 props로 받아 옴.
     return(
         <Router>
+            {islogin && <Navigation />} {/* 로그인시에 만 보임. */}
             <Switch>
                 {islogin ? (
                 <div> 
                     <Route exact path="/">
                          {/* 로그인이 되어있으면 HOME화면을 보여 줌 */}
                         <Home />
+                    </Route>
+                    <Route exact path="/profile">
+                         {/* 로그인이 되어있으면 Profile화면을 보여 줌 */}
+                        <Profile />
                     </Route>
                 </div>
                 ) : (
--- reactcrud/src/routes/Profile.jsx
+++ reactcrud/src/routes/Profile.jsx
@@ -1,10 +1,16 @@
 // //function component
 // export default () => <span>Auth</span>
 
+import { authService } from "fbase";
+
 function Profile () {
+
+    const onLogoutClick = () => {
+        authService.signOut();
+    }
     return(
         <div>
-
+            <button onClick={onLogoutClick}>로그아웃</button>
         </div>
     );
 }
