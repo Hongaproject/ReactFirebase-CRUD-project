e1f44f70cc4c188ab87c7c8a3fc6993dec60ee0b
Hongaproject <tkwlscjq99@gmail.com>
2023-09-01 16:51:20 +0900

    firebase DB와 연동 Home 페이지에 텍스트 입력시 자동 등록

--- reactcrud/src/routes/Home.jsx
+++ reactcrud/src/routes/Home.jsx
@@ -1,20 +1,30 @@
 // //function component
 // export default () => <span>Auth</span>
 
+import { dbService } from "fbase";
 import { useState } from "react";
 
 function Home () {
 
     const [text, setText] = useState("");
+
     const onChange = (e) => {
         const {target: {value}} = e;
         setText(value);
 
         // setText(e.target.value); // 위에 꺼와 같음.
     }
-
+    
     const onSubmit = (e) => {
         e.preventDefault();
+        // firebaseDB는 collection과 document로 구성이 되어 사용이 됨.
+        dbService.collection("texts").add({ 
+        // collection에 texts라는 이름을 넣어줌
+        // add는 document에 자동으로 data값을 넣어 줌.
+        text,
+        create: Date.now(),
+        })
+        setText(""); // text값을 받아 create에 넘겨준다.
     }
 
     return(
@@ -27,7 +37,7 @@ function Home () {
                     onChange={onChange} 
                     value={text}
                 />
-                <input type="submot" value="등록" />
+                <input type="submit" value="등록" />
             </form>
         </div>
     );
