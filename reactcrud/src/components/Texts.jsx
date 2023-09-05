de54a0638ef2fd8eb87a505812b0bb1383e81803
Hongaproject <tkwlscjq99@gmail.com>
2023-09-05 12:37:40 +0900

    삭제 기능 추가

--- reactcrud/src/components/Texts.jsx
+++ reactcrud/src/components/Texts.jsx
@@ -1,13 +1,23 @@
+import { dbService } from "fbase";
 
 function Texts ({userObj, userData}) {
 // userData를 Home에서 props받아 text부분과 userID가 일치한다면 수정 및 삭제를 할 수 있게 함.
+
+    const onDelete = () => { // async,await를 사용 할 수 있다.
+        const deleteAlert= window.confirm("삭제 하시겠습니까?");
+        console.log(deleteAlert); 
+        if(deleteAlert){
+            dbService.doc(`texts/${userObj.id}`).delete(); 
+            // firebase는 폴더 구조로 되어있음 
+        }
+    }
 
     return(
         <div>
             <h4>{userObj.text}</h4>
             { userData && (
                 <>
-                    <button>삭제</button>
+                    <button onClick={onDelete}>삭제</button>
                     <button>수정</button>
                 </>
             )}
